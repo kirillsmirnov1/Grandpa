4d9aa45b8010bc83504db9defd9552abe7aef8a4
kirillsmirnov1 <thegaldren@gmail.com>
2022-12-09 17:02:30 +0400

    extract follow player

--- Assets/Nightmares/Code/Control/CameraFollowsPlayer.cs
+++ Assets/Nightmares/Code/Control/CameraFollowsPlayer.cs
@@ -1,19 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Nightmares.Code.Control
 {
     public class CameraFollowsPlayer : MonoBehaviour
     {
         public Transform target;
+        public TilemapRenderer baseTiles;
 
-        // TODO move with lerp
-        // TODO limits 
+        private Camera _cam;
         
+        private float _yMax;
+        private float _yMin;
+        
+        // TODO move with lerp
+
+        private void Awake()
+        {
+            _cam = GetComponent<Camera>();
+            CalculateLimitsFromTileMap(baseTiles);
+        }
+
         private void Update()
         {
+            FollowPlayer();
+        }
+
+        private void FollowPlayer()
+        {
             var pos = transform.position;
-            pos.y = target.position.y;
+            var newY = Mathf.Clamp(target.position.y, _yMin + _cam.orthographicSize, _yMax - _cam.orthographicSize);
+            pos.y = newY;
             transform.position = pos;
+        }
+
+        private void CalculateLimitsFromTileMap(TilemapRenderer tiles)
+        {
+            _yMax = tiles.bounds.max.y;
+            _yMin = tiles.bounds.min.y;
         }
     }
 }
