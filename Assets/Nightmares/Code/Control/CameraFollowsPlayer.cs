0e2012714a875dc7eebc2d850e961e0fa1902138
kirillsmirnov1 <thegaldren@gmail.com>
2022-12-09 16:44:38 +0400

    simple CameraFollowsPlayer.cs

---
+++ Assets/Nightmares/Code/Control/CameraFollowsPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Nightmares.Code.Control
+{
+    public class CameraFollowsPlayer : MonoBehaviour
+    {
+        public Transform target;
+
+        // TODO move with lerp
+        // TODO limits 
+        
+        private void Update()
+        {
+            var pos = transform.position;
+            pos.y = target.position.y;
+            transform.position = pos;
+        }
+    }
+}
