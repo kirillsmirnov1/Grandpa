2e23465fed0cd18c2b0cf05d91977c619e7b40dd
kirillsmirnov1 <thegaldren@gmail.com>
2022-12-16 14:14:59 +0400

    Some basic enemy movement

---
+++ Assets/Nightmares/Code/Control/FlyEnemyMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Nightmares.Code.Control
+{
+    public class FlyEnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1;
+        [SerializeField] private float dirResetTime = 2f;
+
+        private Rigidbody2D _rb;
+        private float _nextDirectionChange;
+        private Vector2 _direction;
+        
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            InitialDirectionChange();
+        }
+
+        private void Update()
+        {
+            CheckDirectionUpdate();
+            Move();
+        }
+
+        private void CheckDirectionUpdate()
+        {
+            if (Time.time > _nextDirectionChange)
+            {
+                ChangeDirection();
+            }
+        }
+
+        private void Move()
+        {
+            var velocity = _rb.velocity;
+            var velocityDiff = _direction - velocity;
+            var velocityChange = velocityDiff * speed * Time.deltaTime;
+            _rb.velocity += velocityChange;
+        }
+
+        private void InitialDirectionChange()
+        {
+            ChangeDirection();
+            _nextDirectionChange = Time.time + Random.Range(dirResetTime / 3f, dirResetTime);
+        }
+
+        private void ChangeDirection()
+        {
+            _direction = Random.insideUnitCircle.normalized;
+            _nextDirectionChange = Time.time + dirResetTime;
+        }
+    }
+}
