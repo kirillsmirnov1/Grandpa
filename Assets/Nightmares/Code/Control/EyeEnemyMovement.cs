7deac25fe20d4078350a3ee0dc866ae9992c9c8a
kirillsmirnov1 <thegaldren@gmail.com>
2022-12-22 17:21:32 +0400

    Start working on the EyeEnemyMovement.cs

---
+++ Assets/Nightmares/Code/Control/EyeEnemyMovement.cs
@@ -0,0 +1,74 @@
+﻿using Nightmares.Code.Model;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Nightmares.Code.Control
+{
+    public class EyeEnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1;
+        [SerializeField] private float dirResetTime = 2f;
+        [SerializeField] private float eyeRotationSpeed = 3f;
+        
+        private Rigidbody2D _rb;
+        private float _nextDirectionChange;
+        private Vector2 _direction;
+        
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            InitialDirectionChange();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawRay(transform.position, _direction);
+        }
+
+        private void Update()
+        {
+            CheckDirectionUpdate();
+            RotateEye();
+            Move();
+        }
+
+        private void RotateEye()
+        {
+            var desiredRotation = Quaternion.Euler(0, 0, _direction.ToAngleInDegrees()); 
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, eyeRotationSpeed * Time.deltaTime);
+        }
+
+        private void CheckDirectionUpdate()
+        {
+            if (Time.time > _nextDirectionChange)
+            {
+                ChangeDirection();
+            }
+        }
+
+        private void Move()
+        {
+            var velocity = _rb.velocity;
+            var velocityDiff = _direction - velocity;
+            var velocityChange = velocityDiff * speed * Time.deltaTime;
+            _rb.velocity += velocityChange;
+        }
+
+        private void InitialDirectionChange()
+        {
+            ChangeDirection();
+            _nextDirectionChange = Time.time + Random.Range(dirResetTime / 3f, dirResetTime);
+        }
+
+        private void ChangeDirection()
+        {
+            _direction = Random.insideUnitCircle.normalized;
+            _nextDirectionChange = Time.time + dirResetTime;
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            ChangeDirection();
+        }
+    }
+}
---
+++ Assets/Nightmares/Code/Model/MathfUtils.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Nightmares.Code.Model
+{
+    public static class MathfUtils
+    {
+        public static float Angle(Vector2 vector2)
+        {
+            return 360 - (Mathf.Atan2(vector2.x, vector2.y) * Mathf.Rad2Deg * Mathf.Sign(vector2.x));
+        }
+        
+        /// <summary>
+        /// Counts full circle counter clock wise from (1, 0)
+        /// </summary>
+        public static float ToAngleInDegrees(this Vector2 v)
+        {
+            var angle = Vector2.Angle(Vector2.right, v);
+            if (v.y < 0) angle = 360 - angle;
+            return angle;
+        }
+    }
+}
