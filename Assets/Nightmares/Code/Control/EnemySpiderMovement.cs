4a5012d5e445e1247fdb5c8ac6295dd348421c26
kirillsmirnov1 <thegaldren@gmail.com>
2023-04-11 10:32:08 +0400

    start working on spider

---
+++ Assets/Nightmares/Code/Control/EnemySpiderMovement.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Nightmares.Code.Control
+{
+    public class EnemySpiderMovement : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer lineRenderer;
+    }
+}
