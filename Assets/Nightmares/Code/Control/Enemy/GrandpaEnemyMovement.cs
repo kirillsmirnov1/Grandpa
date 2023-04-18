51b5ccb34e505efd4ef5f245e0801908f301caa9
kirillsmirnov1 <thegaldren@gmail.com>
2023-04-18 17:31:15 +0400

    start GrandpaEnemyMovement.cs

---
+++ Assets/Nightmares/Code/Control/Enemy/GrandpaEnemyMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Nightmares.Code.Control.Enemy
+{
+    public class GrandpaEnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 1f;
+        
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private Rigidbody2D staffRb;
+    }
+}
