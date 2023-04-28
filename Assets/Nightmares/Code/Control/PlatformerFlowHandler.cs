15e0b3a8f34fb2e7f5d22a66c1f12e2d81cb3084
kirillsmirnov1 <thegaldren@gmail.com>
2023-04-28 17:57:45 +0400

    restarting from ui

--- Assets/Nightmares/Code/Control/PlatformerFlowHandler.cs
+++ Assets/Nightmares/Code/Control/PlatformerFlowHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DG.Tweening;
+using Nightmares.Code.Control.Enemy;
 using Nightmares.Code.Extensions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,20 +8,47 @@ namespace Nightmares.Code.Control
 {
     public class PlatformerFlowHandler : MonoBehaviour
     {
+        [SerializeField] private CanvasGroup victoryBanner;
+        [SerializeField] private CanvasGroup defeatBanner;
+
+        private bool _gameOverTriggered;
+        
         private void Awake()
         {
             Player.OnPlayerDeath += OnPlayerDeath;
+            GrandpaController.OnGrandpaDefeated += OnVictory;
         }
 
         private void OnDestroy()
         {
             Player.OnPlayerDeath -= OnPlayerDeath;
+            GrandpaController.OnGrandpaDefeated -= OnVictory;
+        }
+
+        private void OnVictory()
+        {
+            if(_gameOverTriggered) return;
+            _gameOverTriggered = true;
+            this.DelayAction(() => ShowBanner(victoryBanner), 1.5f);
         }
 
         private void OnPlayerDeath()
         {
-            // TODO pop-up
-            this.DelayAction(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name), 1.5f);
+            if(_gameOverTriggered) return;
+            _gameOverTriggered = true;
+            this.DelayAction(() => ShowBanner(defeatBanner), 1.5f);
+        }
+
+        private void ShowBanner(CanvasGroup banner)
+        {
+            banner.gameObject.SetActive(true);
+            banner.alpha = 0;
+            banner.DOFade(1f, .5f);
+        }
+
+        public void ReloadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
