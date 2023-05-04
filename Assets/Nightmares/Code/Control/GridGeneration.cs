3ac3af4d1f2c5643e24c07d06bfd904001f9f61a
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-04 15:44:01 +0400

    generate platforms

--- Assets/Nightmares/Code/Control/GridGeneration.cs
+++ Assets/Nightmares/Code/Control/GridGeneration.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 namespace Nightmares.Code.Control
 {
     public class GridGeneration : MonoBehaviour
     {
-        [SerializeField] private Vector2Int dimensions = new Vector2Int(9, 40);
+        [SerializeField] private Vector2Int levelDimensions = new(13, 40);
+        [SerializeField] private Vector2Int verticalGap = new(3, 5);
+
+        [Header("Components")]
         [SerializeField] private Tilemap tilemap;
         
         [Header("Wall Tiles")]
@@ -27,9 +32,12 @@ namespace Nightmares.Code.Control
         public void SpawnTileMap()
         {
             CleanTileMap();
-            SpawnRow(wallTop, -dimensions.x / 2, dimensions.x / 2, 0);
+            
+            SpawnRow(wallTop, -levelDimensions.x / 2, levelDimensions.x / 2, 0);
             SpawnWalls();
-            SpawnRow(wallBottom, -dimensions.x / 2, dimensions.x / 2, dimensions.y);
+            SpawnRow(wallBottom, -levelDimensions.x / 2, levelDimensions.x / 2, levelDimensions.y);
+            
+            SpawnPlatforms();
         }
 
         private void SpawnRow(Tile tile, int xFrom, int xTo, int y)
@@ -42,13 +50,71 @@ namespace Nightmares.Code.Control
 
         private void SpawnWalls()
         {
-            var xLeft = -dimensions.x / 2;
-            var xRight = dimensions.x / 2;
-            for (int y = 1; y < dimensions.y; y++)
+            var xLeft = -levelDimensions.x / 2;
+            var xRight = levelDimensions.x / 2;
+            for (int y = 1; y < levelDimensions.y; y++)
             {
                 tilemap.SetTile(new Vector3Int(xLeft, y), wallLeft);
                 tilemap.SetTile(new Vector3Int(xRight, y), wallRight);
             }
+        }
+
+        private void SpawnPlatforms()
+        {
+            var ranges = GenerateRanges();
+            var nextRangeIndex = 0;
+            for (int y = verticalGap.y; y < levelDimensions.y - verticalGap.y; )
+            {
+                var range = ranges[nextRangeIndex];
+                
+                var left = Random.Range(range.x, range.y);
+                var right = Random.Range(left, range.y + 1);
+
+                SpawnPlatform(left, right, y);
+
+                y += Random.Range(verticalGap.x, verticalGap.y + 1);
+                nextRangeIndex = Random.Range(0, 3);
+            }
+        }
+
+        private void SpawnPlatform(int xLeft, int xRight, int y)
+        {
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                tilemap.SetTile(new Vector3Int(x, y), platformMidTile);
+            }
+        }
+
+        private Vector2Int[] GenerateRanges()
+        {
+            int maxPlatformWidth = MaxPlatformWidth();
+            return new[]
+            {
+                new Vector2Int(-levelDimensions.x / 2 + 1, -levelDimensions.x / 2 + maxPlatformWidth),
+                new Vector2Int(-maxPlatformWidth / 2, maxPlatformWidth / 2),
+                new Vector2Int(levelDimensions.x / 2 - maxPlatformWidth, levelDimensions.x / 2 - 1),
+            };
+        }
+
+        private int MaxPlatformWidth()
+        {
+            var maxPlatformWidth = levelDimensions.x / 3 + 1;
+            if (levelDimensions.x % 2 == 0)
+            {
+                if (maxPlatformWidth % 2 != 0)
+                {
+                    maxPlatformWidth++;
+                }
+            }
+            else
+            {
+                if (maxPlatformWidth % 2 == 0)
+                {
+                    maxPlatformWidth++;
+                }
+            }
+
+            return maxPlatformWidth;
         }
     }
 }
