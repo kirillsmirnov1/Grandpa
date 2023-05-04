a920393fcf28215f191dbf79bb7445cd52513cb2
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-04 14:33:52 +0400

    start grid generation

---
+++ Assets/Nightmares/Code/Control/GridGeneration.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Nightmares.Code.Control
+{
+    public class GridGeneration : MonoBehaviour
+    {
+        [SerializeField] private Tilemap tilemap;
+        [SerializeField] private Tile tile;
+
+        [ContextMenu("Spawn Tile Map")]
+        public void SpawnTileMap()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                tilemap.SetTile(new Vector3Int(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)), tile);
+            }
+        }
+
+        [ContextMenu("Clean Tile Map")]
+        public void CleanTileMap()
+        {
+            tilemap.ClearAllTiles();
+        }
+    }
+}
