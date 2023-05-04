474b80dc4950e9af620461757e5320bcae32edc1
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-04 14:36:14 +0400

    mv functions

--- Assets/Nightmares/Code/Control/GridGeneration.cs
+++ Assets/Nightmares/Code/Control/GridGeneration.cs
@@ -8,19 +8,20 @@ namespace Nightmares.Code.Control
         [SerializeField] private Tilemap tilemap;
         [SerializeField] private Tile tile;
 
-        [ContextMenu("Spawn Tile Map")]
-        public void SpawnTileMap()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                tilemap.SetTile(new Vector3Int(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)), tile);
-            }
-        }
-
         [ContextMenu("Clean Tile Map")]
         public void CleanTileMap()
         {
             tilemap.ClearAllTiles();
+        }
+
+        [ContextMenu("Spawn Tile Map")]
+        public void SpawnTileMap()
+        {
+            CleanTileMap();
+            for (int i = 0; i < 10; i++)
+            {
+                tilemap.SetTile(new Vector3Int(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)), tile);
+            }
         }
     }
 }
