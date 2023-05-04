d474b280eab1188129e7445273bb52e05d80a6ca
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-04 14:51:52 +0400

    wall generation

--- Assets/Nightmares/Code/Control/GridGeneration.cs
+++ Assets/Nightmares/Code/Control/GridGeneration.cs
@@ -5,8 +5,14 @@ namespace Nightmares.Code.Control
 {
     public class GridGeneration : MonoBehaviour
     {
+        [SerializeField] private Vector2Int dimensions = new Vector2Int(9, 40);
         [SerializeField] private Tilemap tilemap;
-        [SerializeField] private Tile tile;
+        
+        [Header("Tiles")]
+        [SerializeField] private Tile wallLeft;
+        [SerializeField] private Tile wallRight;
+        [SerializeField] private Tile wallTop;
+        [SerializeField] private Tile wallBottom;
 
         [ContextMenu("Clean Tile Map")]
         public void CleanTileMap()
@@ -18,9 +24,27 @@ namespace Nightmares.Code.Control
         public void SpawnTileMap()
         {
             CleanTileMap();
-            for (int i = 0; i < 10; i++)
+            SpawnRow(wallTop, -dimensions.x / 2, dimensions.x / 2, 0);
+            SpawnWalls();
+            SpawnRow(wallBottom, -dimensions.x / 2, dimensions.x / 2, dimensions.y);
+        }
+
+        private void SpawnRow(Tile tile, int xFrom, int xTo, int y)
+        {
+            for (int x = xFrom + 1; x < xTo; x++)
             {
-                tilemap.SetTile(new Vector3Int(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)), tile);
+                tilemap.SetTile(new Vector3Int(x, y), tile);
+            }
+        }
+
+        private void SpawnWalls()
+        {
+            var xLeft = -dimensions.x / 2;
+            var xRight = dimensions.x / 2;
+            for (int y = 1; y < dimensions.y; y++)
+            {
+                tilemap.SetTile(new Vector3Int(xLeft, y), wallLeft);
+                tilemap.SetTile(new Vector3Int(xRight, y), wallRight);
             }
         }
     }
