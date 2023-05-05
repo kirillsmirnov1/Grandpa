f3740887d89f4e8ea05267274f3520af75fd12b2
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-05 17:53:21 +0400

    force mobile controls button for webgl

--- Assets/Nightmares/Code/Model/TouchControlsEnabler.cs
+++ Assets/Nightmares/Code/Model/TouchControlsEnabler.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Nightmares.Code.Model
 {
     public class TouchControlsEnabler : MonoBehaviour
     {
         public static bool TouchControlsEnabled 
-            => _instance.forceMobileControls || Application.platform == RuntimePlatform.Android;
+            => _instance.forceMobileControls 
+               || Application.platform == RuntimePlatform.Android
+               || Prefs.ForcedMobileControls;
         
         private static TouchControlsEnabler _instance;
         
         public bool forceMobileControls = false;
 
         [SerializeField] private GameObject mobileCanvas;
+        [SerializeField] private Button forceEnableMobileControlsButton;
         
         private void Awake()
         {
@@ -28,7 +32,21 @@ namespace Nightmares.Code.Model
 
         private void EnableMobileCanvas()
         {
+#if UNITY_WEBGL
+            forceEnableMobileControlsButton.gameObject.SetActive(!Prefs.ForcedMobileControls);
+            forceEnableMobileControlsButton.onClick.AddListener(OnForceEnableMobileControlsButtonClick);
+#else
+            forceEnableMobileControlsButton.gameObject.SetActive(false);
+#endif
             mobileCanvas.SetActive(TouchControlsEnabled);
         }
+#if UNITY_WEBGL
+        private void OnForceEnableMobileControlsButtonClick()
+        {
+            Prefs.ForcedMobileControls = true;
+            forceEnableMobileControlsButton.gameObject.SetActive(false);
+            EnableMobileCanvas();
+        }
+#endif
     }
 }
