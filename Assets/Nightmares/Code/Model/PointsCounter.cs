f3c57bd0d9ba73ff3d905211cbd72589f99ab3d8
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-05 16:57:25 +0400

    points counter

---
+++ Assets/Nightmares/Code/Model/PointsCounter.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Nightmares.Code.Model
+{
+    public class PointsCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI pointsText;
+        
+        public int Points
+        {
+            get => _points;
+            private set
+            {
+                if(value == _points) return;
+                _points = value;
+                pointsText.text = _points.ToString();
+            }
+        }
+
+        private int _points = int.MinValue;
+
+        private void Awake()
+        {
+            Points = 0;
+        }
+    }
+}
