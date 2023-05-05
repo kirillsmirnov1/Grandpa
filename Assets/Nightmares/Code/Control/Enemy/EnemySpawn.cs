2ca42add0731e3a6b5b9f910d3ffe07bd8bce521
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-05 14:24:46 +0400

    first attempt at dynamic enemy spawn

--- Assets/Nightmares/Code/Control/Enemy/EnemySpawn.cs
+++ Assets/Nightmares/Code/Control/Enemy/EnemySpawn.cs
@@ -5,16 +5,32 @@ namespace Nightmares.Code.Control.Enemy
     public class EnemySpawn : MonoBehaviour
     {
         [SerializeField] private GameObject[] enemyPrefabs;
+        [SerializeField] private int[] spawnCost;
+        [SerializeField] private AnimationCurve budget;
         [SerializeField] private PlatformerGameManager gameManager;
+
+        private float _balance;
         
         public void SpawnEnemies()
         {
             var levelDimensions = gameManager.LevelDimensions;
-            for (int y = 3; y < levelDimensions.y - 5; y++)
+            float yLimit = levelDimensions.y;
+            for (float y = 0; y < yLimit; y++)
+            {
+                _balance += budget.Evaluate(y / yLimit);
+                while (_balance >= 1)
+                {
+                    var enemyIndex = Random.Range(0, enemyPrefabs.Length);
+                    _balance -= spawnCost[enemyIndex];
+                    SpawnEnemy(enemyIndex, -y);
+                }
+            }
+
+            void SpawnEnemy(int enemyIndex, float yPos)
             {
                 Instantiate(
-                    enemyPrefabs[Random.Range(0, enemyPrefabs.Length)],
-                    new Vector3(Random.Range(-levelDimensions.x / 2f + 1, levelDimensions.x / 2f - 1), -y),
+                    enemyPrefabs[enemyIndex],
+                    new Vector3(Random.Range(-levelDimensions.x / 2f + 1, levelDimensions.x / 2f - 1), yPos),
                     Quaternion.identity,
                     transform);
             }
