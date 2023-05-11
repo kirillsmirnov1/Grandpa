260394f2d130f32701375c74f76c9b24bde8e972
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-11 15:04:14 +0400

    extract function

--- Assets/Nightmares/Code/Control/CameraFollowsPlayer.cs
+++ Assets/Nightmares/Code/Control/CameraFollowsPlayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Nightmares.Code.Control
 {
@@ -13,17 +14,19 @@ namespace Nightmares.Code.Control
         
         private float _yMax;
         private float _yMin;
-        
+        private Action _onFixedUpdate;
+
         // TODO move with lerp
 
         private void Awake()
         {
             _cam = GetComponent<Camera>();
+            _onFixedUpdate = FollowPlayer;
         }
 
         private void FixedUpdate()
         {
-            FollowPlayer(Time.fixedDeltaTime);
+            _onFixedUpdate?.Invoke();
         }
 
         public void InitDimensions()
@@ -32,12 +35,12 @@ namespace Nightmares.Code.Control
             _yMin = -gameManager.LevelDimensions.y - gameManager.GrandpasRoomHeight;
         }
 
-        private void FollowPlayer(float dt)
+        private void FollowPlayer()
         {
             var pos = transform.position;
             var newY = Mathf.Clamp(target.position.y, _yMin + _cam.orthographicSize, _yMax - _cam.orthographicSize);
             pos.y = newY;
-            transform.position = Vector3.Lerp(transform.position, pos, lerpSpeed * dt);
+            transform.position = Vector3.Lerp(transform.position, pos, lerpSpeed * Time.fixedDeltaTime);
         }
     }
 }
