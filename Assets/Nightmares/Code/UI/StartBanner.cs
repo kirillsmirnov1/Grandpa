c97f72cf1709d5bc68afaabb17d86b3233763f94
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-15 19:06:25 +0400

    spawn numbers

--- Assets/Nightmares/Code/UI/StartBanner.cs
+++ Assets/Nightmares/Code/UI/StartBanner.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,11 @@ namespace Nightmares.Code.UI
 {
     public class StartBanner : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI numberPrefab;
+        
+        [Header("Components")]
+        [SerializeField] private CenterScroll difficultyCenterScroll;
+        [SerializeField] private RectTransform difficultyScrollContent;
         [SerializeField] private Button startButton;
 
         private Action _startCallback;
@@ -13,6 +19,12 @@ namespace Nightmares.Code.UI
         public void Show(Action startCallback)
         {
             _startCallback = startCallback;
+            for (int i = 0; i < 5; i++) // TODO use max difficulty unlocked
+            {
+                var letter = Instantiate(numberPrefab, difficultyScrollContent);
+                letter.text = $"{i + 1}";
+            }
+            difficultyCenterScroll.enabled = true;
             gameObject.SetActive(true);
         }
         
