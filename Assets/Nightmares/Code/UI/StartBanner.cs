c8c42ca0f0805b6e6abb9a468806598f44cc2c0c
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-16 14:14:05 +0400

    wall scroll

--- Assets/Nightmares/Code/UI/StartBanner.cs
+++ Assets/Nightmares/Code/UI/StartBanner.cs
@@ -7,9 +7,14 @@ namespace Nightmares.Code.UI
 {
     public class StartBanner : MonoBehaviour
     {
+        [Header("Params")]
+        [SerializeField] private float wallSpeed = 1;
+        
+        [Header("Prefabs")]
         [SerializeField] private TextMeshProUGUI numberPrefab;
         
-        [Header("Components")]
+        [Header("Components")] 
+        [SerializeField] private RawImage[] walls;
         [SerializeField] private CenterScroll difficultyCenterScroll;
         [SerializeField] private RectTransform difficultyScrollContent;
         
@@ -51,6 +56,14 @@ namespace Nightmares.Code.UI
             if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) OnRightButtonClick();
             if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) OnLeftButtonClick();
             if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) OnStartButtonClick();
+
+            var wallDist = Time.deltaTime * wallSpeed;
+            foreach (var wall in walls)
+            {
+                var rect = wall.uvRect;
+                rect.y += wallDist;
+                wall.uvRect = rect;
+            }
         }
 
         private void OnRightButtonClick() => ScrollDifficulty(1);
