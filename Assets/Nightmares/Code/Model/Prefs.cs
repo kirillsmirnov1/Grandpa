e3729e59879622d6fcdc84da53a2d5f6b0389519
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-16 14:41:24 +0400

    max unlocked difficulty

--- Assets/Nightmares/Code/Model/Prefs.cs
+++ Assets/Nightmares/Code/Model/Prefs.cs
@@ -6,11 +6,18 @@ namespace Nightmares.Code.Model
     public static class Prefs
     {
         private const string KeyGrandpaDifficulty = "KeyGrandpaDifficulty";
+        private const string KeyGrandpaDifficultyMax = "KeyGrandpaDifficultyMax";
 
         public static int GrandpaDifficulty
         {
             get => Mathf.Clamp(PlayerPrefs.GetInt(KeyGrandpaDifficulty, 1), 1, Constants.GrandpaMaxDifficulty);
             set => PlayerPrefs.SetInt(KeyGrandpaDifficulty, Mathf.Clamp(value, 1, Constants.GrandpaMaxDifficulty));
+        }
+        
+        public static int GrandpaDifficultyMaxUnlocked
+        {
+            get => Mathf.Clamp(PlayerPrefs.GetInt(KeyGrandpaDifficultyMax, 1), 1, Constants.GrandpaMaxDifficulty);
+            set => PlayerPrefs.SetInt(KeyGrandpaDifficultyMax, Mathf.Clamp(value, 1, Constants.GrandpaMaxDifficulty));
         }
     }
 }
