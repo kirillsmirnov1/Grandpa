bcf70f06e3a3eff2e6c58a7e413d9f17efb33cbe
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-23 15:47:28 +0400

    add UID to quest

--- Assets/Nightmares/Code/Model/Quests/Quest.cs
+++ Assets/Nightmares/Code/Model/Quests/Quest.cs
@@ -1,5 +1,8 @@
 ﻿using System.Linq;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Nightmares.Code.Model.Quests
 {
@@ -10,6 +13,22 @@ namespace Nightmares.Code.Model.Quests
         [SerializeField] private int minLevel;
         [SerializeField] private QuestTask[] tasks;
 
+        [SerializeField, HideInInspector] private string guid;
+        
         public bool Complete => tasks.All(t => t.Complete);
+        public string UID => guid;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            CheckGuidSerialization();
+        }
+
+        private void CheckGuidSerialization()
+        {
+            guid = AssetDatabase.GUIDFromAssetPath(AssetDatabase.GetAssetPath(this)).ToString();
+            EditorUtility.SetDirty(this);
+        }
+#endif
     }
 }
