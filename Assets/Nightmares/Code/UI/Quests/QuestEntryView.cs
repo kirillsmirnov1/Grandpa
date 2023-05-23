73d22f6444c028aa98c4c834edb10abb2551cd6d
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-23 18:19:41 +0400

    QuestEntryView.cs

--- Assets/Nightmares/Code/UI/Quests/QuestEntryView.cs
+++ Assets/Nightmares/Code/UI/Quests/QuestEntryView.cs
@@ -1,9 +1,24 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Nightmares.Code.UI.Quests
 {
     public class QuestEntryView : MonoBehaviour
     {
+        [Header("Sprites")]
+        [SerializeField] private Sprite questComplete;
+        [SerializeField] private Sprite questIncomplete;
 
+        [Header("Components")]
+        [SerializeField] private Image statusImage;
+        [SerializeField] private TextMeshProUGUI questText;
+
+        public void Set(bool unlocked, bool completed, string txt)
+        {
+            statusImage.gameObject.SetActive(unlocked);
+            statusImage.sprite = completed ? questComplete : questIncomplete;
+            questText.text = unlocked ? txt : "???????????????";
+        }
     }
 }
