d043e59976a4c518abda563f27512b86eee7c795
kirillsmirnov1 <thegaldren@gmail.com>
2023-05-23 15:43:38 +0400

    create QuestManager.cs

---
+++ Assets/Nightmares/Code/Model/Quests/QuestManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Nightmares.Code.Model.Quests
+{
+    [CreateAssetMenu(fileName = "New Quest Manager", menuName = "Data/Quest Manager", order = 0)]
+    public class QuestManager : ScriptableObject
+    {
+        /// <summary>
+        /// All available quests
+        /// </summary>
+        [SerializeField] private Quest quests;
+    }
+}
