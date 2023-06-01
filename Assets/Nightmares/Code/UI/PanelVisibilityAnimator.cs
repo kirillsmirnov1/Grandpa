128e6a04f046942092420ce7df0c689a57845085
kirillsmirnov1 <thegaldren@gmail.com>
2023-06-01 11:29:15 +0400

    extract PanelVisibilityAnimator.cs

---
+++ Assets/Nightmares/Code/UI/PanelVisibilityAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Nightmares.Code.UI
+{
+    public class PanelVisibilityAnimator : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private RectTransform panel;
+
+        [Header("Animation")]
+        [SerializeField] private float animDuration = .3f;
+        [Range(0f, 1f)]
+        [SerializeField] private float lowering = .3f;
+
+        private Sequence _animSeq;
+
+        public void Show() => Show(null);
+        public void Show(Action callback) => Animate(true, callback);
+
+        public void Hide() => Hide(null);
+        public void Hide(Action callback)
+            => Animate(false, () =>
+            {
+                gameObject.SetActive(false);
+                callback?.Invoke();
+            });
+        
+        private void Animate(bool show, Action callback = null) // TODO extract as a component 
+        {
+            _animSeq?.Complete();
+         
+            canvasGroup.alpha = show ? 0f : 1f;
+            var dY = panel.rect.height * lowering;
+            panel.anchoredPosition = show ? panel.anchoredPosition - new Vector2(0, dY) : Vector2.zero;
+            
+            _animSeq = DOTween.Sequence()
+                .Append(canvasGroup.DOFade(show ? 1f : 0f, animDuration))
+                .Join(panel.DOAnchorPosY(show ? 0 : -dY, animDuration))
+                .AppendCallback(() => callback?.Invoke());
+        }
+    }
+}
--- Assets/Nightmares/Code/UI/Quests/QuestListView.cs
+++ Assets/Nightmares/Code/UI/Quests/QuestListView.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Linq;
-using DG.Tweening;
 using Nightmares.Code.Model.Quests;
 using UnityEngine;
 using UnityUtils.Variables;
@@ -8,21 +6,14 @@ using UnityUtils.View;
 
 namespace Nightmares.Code.UI.Quests
 {
+    [RequireComponent(typeof(PanelVisibilityAnimator))]
     public class QuestListView : ListView<QuestDisplayData>
     {
-        [SerializeField] private CanvasGroup canvasGroup;
-        [SerializeField] private RectTransform panel;
-        
+        [SerializeField] private PanelVisibilityAnimator panelVisibilityAnimator;
+
         [Header("Resources")]
         [SerializeField] private QuestManager quests;
         [SerializeField] private IntVariable maxUnlockedLevel;
-
-        [Header("Animation")]
-        [SerializeField] private float animDuration = .3f;
-        [Range(0f, 1f)]
-        [SerializeField] private float lowering = .3f;
-
-        private Sequence _animSeq;
 
         public void Show()
         {
@@ -34,26 +25,12 @@ namespace Nightmares.Code.UI.Quests
             gameObject.SetActive(true);
             SetEntries(questsToShow.Select(q => q.ToDisplayData(maxUnlockedLevel)).ToList());
 
-            Animate(true);
+            panelVisibilityAnimator.Show();
         }
 
         public void Hide()
         {
-            Animate(false, () => gameObject.SetActive(false));
-        }
-
-        private void Animate(bool show, Action callback = null) // TODO extract as a component 
-        {
-            _animSeq?.Complete();
-         
-            canvasGroup.alpha = show ? 0f : 1f;
-            var dY = panel.rect.height * lowering;
-            panel.anchoredPosition = show ? panel.anchoredPosition - new Vector2(0, dY) : Vector2.zero;
-            
-            _animSeq = DOTween.Sequence()
-                .Append(canvasGroup.DOFade(show ? 1f : 0f, animDuration))
-                .Join(panel.DOAnchorPosY(show ? 0 : -dY, animDuration))
-                .AppendCallback(() => callback?.Invoke());
+            panelVisibilityAnimator.Hide();
         }
     }
 }
