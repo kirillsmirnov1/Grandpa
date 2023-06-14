e6e4ddfda0a2f0ed45d002586e94eb517f1c5e3f
kirillsmirnov1 <thegaldren@gmail.com>
2023-06-14 17:57:04 +0400

    input

--- Assets/Nightmares/Code/UI/Story/StoryView.cs
+++ Assets/Nightmares/Code/UI/Story/StoryView.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Nightmares.Code.Model;
 using UnityEngine;
 using UnityUtils.View;
@@ -8,10 +9,30 @@ namespace Nightmares.Code.UI.Story
     public class StoryView : ListView<StoryEntryData>
     {
         [SerializeField] private List<StoryEntryData> dataEntries;
+        [SerializeField] private CenterScroll centerScroll;
         
         private void OnEnable()
         {
             SetEntries(dataEntries);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                centerScroll.ScrollBy(-1);
+            } 
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                centerScroll.ScrollBy(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                centerScroll.ScrollTo(0);
+            }
+            {
+                
+            }
         }
     }
 }
