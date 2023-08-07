6d76c3dca1d11ef0016f2a411aec2d67a97c45b7
kirillsmirnov1 <thegaldren@gmail.com>
2023-08-07 16:28:36 +0400

    story tex spacing fix

--- Assets/Nightmares/Code/UI/Story/StoryViewEntry.cs
+++ Assets/Nightmares/Code/UI/Story/StoryViewEntry.cs
@@ -1,6 +1,8 @@
-﻿using Nightmares.Code.Model;
+﻿using System.Linq;
+using Nightmares.Code.Model;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityUtils.View;
 
 namespace Nightmares.Code.UI.Story
@@ -10,13 +12,30 @@ namespace Nightmares.Code.UI.Story
         [SerializeField] private TextMeshProUGUI header;
         [SerializeField] private TextMeshProUGUI mainText;
         [SerializeField] private GameObject blurObject;
+        [SerializeField] private LocalizedString mainTextSpacingStr;
         
         public override void Fill(StoryEntryData data)
         {
             header.text = data.title.GetLocalizedString();
             mainText.text = data.mainText.GetLocalizedString();
+
+            SetSpacings();
+
             blurObject.SetActive(!data.unlocked);
             base.Fill(data);
+        }
+
+        private void SetSpacings()
+        {
+            // TODO there must be a better way to do this 
+            var spacings = mainTextSpacingStr
+                .GetLocalizedString()
+                .Split(",")
+                .Select(float.Parse)
+                .ToArray();
+
+            mainText.characterSpacing = spacings[0];
+            mainText.wordSpacing = spacings[1];
         }
     }
 }
