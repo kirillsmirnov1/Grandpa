e1c20b7d6ad689cc9f2105fe31ee0ea014aa9151
kirillsmirnov1 <thegaldren@gmail.com>
2023-08-14 13:35:58 +0400

    wrap words

---
+++ Assets/Nightmares/Code/Model/WrapLocalization.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor.Localization;
+using UnityEngine;
+using UnityEngine.Localization.Tables;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Nightmares.Code.Model
+{
+    [CreateAssetMenu(fileName = "WrapLocalization", menuName = "Data/WrapLocalization", order = 0)]
+    public class WrapLocalization : ScriptableObject
+    {
+        public string wrap = "a{0}b";
+        public string[] stringsToWrap;
+        public LocalizationTableCollection table;
+        
+        public void Wrap()
+        {
+            // TODO 
+            foreach (var subTable in table.Tables)
+            {
+                var asset = subTable.asset as StringTable;
+                foreach (var kv in asset.Values)
+                {
+                    Debug.Log(kv.Value);
+                    foreach (var s in stringsToWrap)
+                    {
+                        kv.Value = kv.Value.Replace(s, string.Format(wrap, s));
+                    }
+                }
+            }
+        }
+    }
+    
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(WrapLocalization))]
+    public class WrapLocalizationEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Wrap"))
+            {
+                (target as WrapLocalization).Wrap();
+            }
+        }
+    }
+#endif
+}
