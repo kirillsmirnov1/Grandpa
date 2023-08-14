c63b9d5788e9fc0a3f0d7ad7d1c1b11fbf9c46c8
kirillsmirnov1 <thegaldren@gmail.com>
2023-08-14 12:35:05 +0400

    fix story ui scaling

--- Assets/Nightmares/Code/UI/Story/StoryViewEntry.cs
+++ Assets/Nightmares/Code/UI/Story/StoryViewEntry.cs
@@ -9,11 +9,23 @@ namespace Nightmares.Code.UI.Story
 {
     public class StoryViewEntry : ListViewEntry<StoryEntryData>
     {
+        [SerializeField] private RectTransform rect;
         [SerializeField] private TextMeshProUGUI header;
         [SerializeField] private TextMeshProUGUI mainText;
         [SerializeField] private GameObject blurObject;
         [SerializeField] private LocalizedString mainTextSpacingStr;
-        
+
+        private void OnEnable()
+        {
+            var viewRect = transform.parent.parent.GetComponent<RectTransform>();
+            var targetSize = viewRect.rect.size;
+            var parentsAspect = targetSize.x / targetSize.y;
+            if (parentsAspect < .7f)
+            {
+                rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rect.rect.height * parentsAspect);
+            }
+        }
+
         public override void Fill(StoryEntryData data)
         {
             header.text = data.title.GetLocalizedString();
